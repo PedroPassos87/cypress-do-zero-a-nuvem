996efc38f1146cbe67eec293fa7f164935cf607b
PedroPassos87 <peagacarreira@gmail.com>
2026-06-14 20:42:13 -0300

    testando report

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,16 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', 
+      overwrite: false,
+      html: true,                   
+      json: true,
+      timestamp: 'mmddyyyy_HHMMss'
+    }
   },
 
   viewportHeight: 880,
